942d3bcf2a4cfbe888789f8563ef6c530dcd86a5
uzair <uzairgithub@gmail.com>
2023-02-19 11:51:22 +0500

    Adding Courses correctly........!

--- src/ApiRequests/ApiRequest.js
+++ src/ApiRequests/ApiRequest.js
@@ -1,107 +1,88 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 function ApiRequest() {
-  const [crscode, setCrscode] = useState("course");
-  const [title, setTitle] = useState("sahdjk");
-  const [instructor, setInstructor] = useState("jasd");
-  const [total_seats, setTotalSeats] = useState(1);
-  const [deadline, setDeadline] = useState("2019-04-28T14:45:15");
-  const [sessions_allowed, setSessionsAllowed] = useState("jasd");
+  const [user, setUser] = useState({
+    crscode: "crscode",
+    title: "title",
+    instructor: "instructor",
+    total_seats: "",
+    deadline: "2019-04-28T14:45:15",
+    sessions_allowed: "sessions_allowed",
+  });
 
-  const handleSubmit = async (e) => {
+  const changeHandler=(e)=>{
+    const {name,value}=e.target;
+    setUser({...user,[name]:value});
+    console.log(user);
+}
+  
+  const submit = async (e) => {
     e.preventDefault();
-    // console.log(crscode);
-    // console.log(title);
-    // console.log(instructor);
-    // console.log(total_seats);
-    // console.log(deadline);
-    // console.log(sessions_allowed);
-
-    const newCourse = {
-      crscode: crscode,
-      title: title,
-      instructor: instructor,
-      total_seats: total_seats,
-      deadline: deadline,
-      sessions_allowed: sessions_allowed,
-    };
-    const json = JSON.stringify(newCourse);
-    console.log("Course code:", newCourse.crscode);
-
-    ////Posting Correctly.......................
-    //   axios.post('http://localhost:3001/admin/add-course', { json })
-    //     .then(res=>{
-    //       console.log(res);
-    //       console.log(res.data);
-    //       // window.location = "/" //This line of code will redirect you once the submission is succeed
-    //     })
-
+    console.log(user);
     const response = await axios({
       method: "post",
       url: "http://localhost:3001/admin/add-course",
-      data: { newCourse },
+      data: user,
       // headers: { "Content-Type": "multipart/form-data" },
     });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Course Name:</label>
-      <input
-        type="text"
-        name="title"
-        value={title}
-        required
-        onChange={(e) => setTitle(e.target.value)}
-      />
+    <div>
+      <h2>Add Course: </h2>
 
-      <label>Course Code:</label>
-      <input
-        type="text"
-        name="crscode"
-        required
-        value={crscode}
-        onChange={(e) => setCrscode(e.target.value)}
-      />
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          name="crscode"
+            value={user.crscode}
+          onChange={changeHandler}
+       
 
-      <label>Instructor name:</label>
-      <input
-        type="text"
-        name="instructor_name"
-        value={instructor}
-        required
-        onChange={(e) => setInstructor(e.target.value)}
-      />
+        />
+        <input
+          type="text"
+          name="title"
+          value={user.title}
+          onChange={changeHandler}
+        
 
-      <label>Total Seats:</label>
-      <input
-        type="number"
-        name="total_seats"
-        value={total_seats}
-        required
-        onChange={(e) => setTotalSeats(e.target.value)}
-      />
+        />
+        <input
+          type="text"
+          name="instructor"
+          value={user.instructor}
+          onChange={changeHandler}
+       
 
-      <label>Deadline:</label>
-      <input
-        type="date"
-        name="deadline"
-        value={deadline}
-        required
-        onChange={(e) => setDeadline(e.target.value)}
-      />
-      <label>Allowed Sessions</label>
-      <input
-        type="text"
-        name="sessions"
-        required
-        value={sessions_allowed}
-        onChange={(e) => setSessionsAllowed(e.target.value)}
-      />
-      <button type="submit">Submit form</button>
-    </form>
+        />
+        <input
+          type="number"
+          name="total_seats"
+          value={user.total_seats}
+          onChange={changeHandler}
+          
+
+        />
+        <input
+          value={user.sessions_allowed}
+          type="text"
+          name="sessions_allowed"
+          onChange={changeHandler}
+         
+        />
+        <input
+          type="date"
+          name="deadline"
+          value={user.deadline}
+          onChange={changeHandler}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
